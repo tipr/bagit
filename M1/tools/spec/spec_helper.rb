5c5d25bff7c0e453ee6e7b83a4d4f43f42262d4c
Marly Wilson <marlified@gmail.com>
2009-01-21 11:40:38 -0500

    Updating tipr descriptor spec, tipr erb, and adding a spec helper class

--- M1/tools/spec/spec_helper.rb
+++ M1/tools/spec/spec_helper.rb
@@ -25,6 +25,42 @@ module TIPRMatchers
 
   end
 
+  class ReferenceAFile
+
+    # Assume an XML file, unless otherwise specified.
+    def initialize(pattern=/\.xml\Z/i, type='XML')  
+      @pattern = pattern        # The REGEX we're searching for
+      @type = type		# A pretty name to describe the file type
+    end
+
+    def matches?(target)
+      @target = target          # a node with an href
+      if @target == nil
+        return false
+      end
+      @ref = @target['href']
+      if @ref == nil 
+        return false
+      end	
+      @ref.match(@pattern) != nil
+    end
+
+    def failure_message
+      "expected <#{@ref}> to be " +
+        "a(n) <#{@type}> file"
+    end
+
+    def negative_failure_message
+      "expected <#{@ref}> not to be " +
+        "a(n) <#{@type}> file"
+    end
+
+  end
+
+  def reference_an_xml_file()
+    ReferenceAFile.new()
+  end
+
   def be_in_namespace(expected)
     BeInNamespace.new(expected)
   end
--- M1/tools/spec/tipr_descriptor_spec.rb
+++ M1/tools/spec/tipr_descriptor_spec.rb
@@ -19,6 +19,12 @@ describe "the tipr descriptor" do
     # code to complete the template
     raw_xml = template.result binding
     @doc = Nokogiri::XML raw_xml   
+
+    # some additional instance variables to help clean up the code
+    @xmlns = { 'xmlns' => 'http://www.loc.gov/METS/' }  
+    @rchildren = @doc.root.children.select { |child| child.name != 'text'}
+    @divs = @doc.root.xpath('//xmlns:structMap/xmlns:div/xmlns:div', @xmlns)
+    @files = @doc.root.xpath('//xmlns:fileSec//xmlns:file', @xmlns)
   end
   
   it "should be a mets document" do
@@ -26,41 +32,109 @@ describe "the tipr descriptor" do
     @doc.root.should be_in_namespace('http://www.loc.gov/METS/')
   end
   
-  it "should have an OBJID"
-  it "should have a LABEL"
+  it "should have an OBJID" do
+    @doc.root.attributes['OBJID'].should_not == nil
+  end
 
-  it "should have a mets header"
+  it "should have a LABEL" do
+    @doc.root.attributes['LABEL'].should_not == nil
+  end
+
+  it "should have a mets header" do
+    @rchildren.first.name.should == 'metsHdr'
+  end
 
   describe "the header" do
-    it "should have a create date"
-    it "should have an agent"
-
-    describe "the agent" do
-      it "should have a role of DISSEMINATOR"
-      it "should have a type of ORGANIZATION"
-      it "should have name of the contributing repository"
+    
+    it "should have a create date" do
+      @rchildren.first['CREATEDATE'].should_not == nil
+    end
+    
+    it "should have an agent" do
+      @rchildren.first.xpath('./xmlns:agent', @xmlns).empty?.should == false
     end
 
+    describe "the agent" do
+      it "should have a role of DISSEMINATOR" do
+        @rchildren.first.xpath('./xmlns:agent', @xmlns).first['ROLE'].should ==
+	  "DISSEMINATOR"
+      end  
+      
+      it "should have a type of ORGANIZATION" do
+        @rchildren.first.xpath('./xmlns:agent', @xmlns).first['TYPE'].should ==
+	  "ORGANIZATION"
+      end
+      
+      it "should have name of the contributing repository" do
+        @rchildren.first.xpath('./xmlns:agent/xmlns:name',
+				@xmlns).first.content.should_not == nil
+      end    
+    end
   end
 
-  it "should not have a dmdSec"
-  it "should have an amdSec"
+  it "should not have a dmdSec" do
+    @doc.root.xpath('//xmlns:dmdSec', @xmlns).empty?.should == true
+  end
+  it "should have an amdSec" do
+    @doc.root.xpath('//xmlns:amdSec', @xmlns).empty?.should == false
+  end
 
   describe "the amdSec" do
-    it "should not have a techMD"
-    it "should not have a sourceMD"
-    it "should have a rightsMD that references an xml file"
-    it "should have a digiprovMD that references an xml file"
+    it "should not have a techMD" do
+      @doc.root.xpath('//xmlns:amdSec/xmlns:techMD', @xmlns).empty?.should ==
+        true
+    end
+    
+    it "should not have a sourceMD" do
+      @doc.root.xpath('//xmlns:amdSec/xmlns:sourceMD', @xmlns).empty?.should ==
+        true
+    end
+    
+    it "should have a rightsMD that references an xml file" do
+      @doc.root.xpath('//xmlns:amdSec/xmlns:rightsMD/xmlns:mdRef', 
+      		@xmlns).first.should reference_an_xml_file
+    end
+    
+    it "should have a digiprovMD that references an xml file" do
+      @doc.root.xpath('./xmlns:amdSec/xmlns:digiprovMD/xmlns:mdRef', 
+      		@xmlns).first.should reference_an_xml_file
+    end    
   end
 
-  it "should have a fileSec that points to representation descriptors"
-  it "should have a struct map"
+  it "should have a fileSec that points to representation descriptors" do
+    # Validate each file representation descriptor.
+    @files.each do |f|
+      f['ID'].should_not == nil
+      f['CHECKSUM'].should_not == nil
+      f['CHECKSUMTYPE'].should == 'SHA-1'
+      f.xpath('./xmlns:FLocat', @xmlns).first.should reference_an_xml_file      
+    end    
+  end
+
+  it "should have a struct map" do
+    @doc.root.xpath('//xmlns:structMap', @xmlns).empty?.should == false
+  end
 
   describe "the struct map" do
-    it "should have at least two divs"
-    it "should have all divs be ordered"
-    it "should have one div labeled active"
-    it "should have a file pointer for each file in the filesec"
+    it "should have at least two divs" do
+      @divs.size.should >= 2
+    end
+
+    it "should have all divs be ordered" do
+      orders = @divs.select { |div| div['ORDER'] != nil}     # Exclude unordered
+      o = orders.map { |d| Integer(d['ORDER']) }             # Map to ints
+      1.upto(@divs.size) { |i| o.include?(i).should == true }# Verify content
+    end
+
+    it "should have exactly one div labeled active" do
+      active = @divs.select { |div| div['TYPE']=='ACTIVE' }
+      active.length.should == 1
+    end
+
+    it "should have a file pointer for each file in the filesec" do
+      fptrs = @divs.xpath('./xmlns:fptr', @xmlns).map { |fp| fp['FILEID'] }
+      @files.each { |f| fptrs.include?(f['ID']).should == true }   
+    end
   end
 
 end
