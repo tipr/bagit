7c063099a1d21c001dc9c7141bfca7dc15b08e73
Francesco Lazzarino <flazzarino@gmail.com>
2009-01-27 17:33:32 -0500

    manifest file created

--- tools/lib/bagit.rb
+++ tools/lib/bagit.rb
@@ -8,9 +8,14 @@ class Bagit
 
   def save(path)
     FileUtils::mkdir path
-
+    
+    # data dir
     data_path = File.join path, 'data'
     FileUtils::mkdir data_path
+
+    # manifest file
+    manifest_path = File.join path, 'manifest-sha1.txt'
+    FileUtils.touch manifest_path
   end
   
 end
--- tools/spec/bagit_spec.rb
+++ tools/spec/bagit_spec.rb
@@ -10,25 +10,25 @@ describe Bagit do
     @sandbox_path = tf.path
     tf.close!
     FileUtils::mkdir @sandbox_path
-    
+
     # make some source data
     source_data_path = File.join @sandbox_path, 'source_data'
     FileUtils::mkdir source_data_path
-    
+
     open('/dev/random') do |rio|
-      
+
       10.times do
-        
+
         Tempfile.open('content-file',  source_data_path) do |tio|
           data = rio.read 16
           tio.write data
           tio.flush
         end
-        
+
       end
-      
+
     end
-    
+
     # make the bag
     @bag_path = File.join @sandbox_path, 'the_bag'
     @bag = Bagit.new source_data_path
@@ -39,67 +39,74 @@ describe Bagit do
     # cleanup
     FileUtils::rm_rf @sandbox_path
   end
-  
+
   it "should be a directory" do
     File.directory?(@bag_path).should be_true
-    
   end
-  
+
   it "should have a sub-directory called data" do
     data_path = File.join @bag_path, 'data'
     File.directory?(data_path).should be_true
   end
-  
-  it "should have at least one manifest-[algorithm].txt"
+
+  it "should have at least one manifest-[algorithm].txt" do
+    pattern = File.join @bag_path, 'manifest-*.txt'
+    Dir.glob(pattern).should_not be_empty
+  end
+
   it "should have a file bagit.txt"
+
+  describe "bagit.txt" do
+    it "should have exaclty two lines"
+    it "should have a bagit version"
+    it "should have a tag file encoding"
+  end
+
   it "may have zero ormore additional files"
+
+  describe "manifest-[algorithm].txt" do
+    it "should have valid algorithm in the name (at least md5 and sha1)"
+    it "should only contain lines of the format CHECKSUM FILENAME"
+    it "should only use the slash character as a path separator in FILENAME"
+    it "should only use hex-encoded checksums"
+    it "should contain FILENAMEs that are relative paths from the base directory"
+    it "should have one or more whitespace characters separating fields"
+  end
+
+  describe "fetch.txt" do
+    it "should only contain lines of the format URL LENGTH FILENAME"
+    it "should only have cannonical URLs"
+    it "should only have a positive integer or - (unspecified) for LENGTH"
+    it "should only use the slash character as a path separator in FILENAME"
+    it "should contain FILENAMEs that are relative paths from the base directory"
+    it "should have one or more whitespace characters separating fields"
+    it "should be renamed when receipt of a bag is complete"
+  end
+
+  describe "tagmanifest-[algorithm].txt" do
+    it "should work just like a nomral manifest file, but only contain tag files"
+  end
+
+  describe "a valid bag" do
+    it "should have every present payload file manifested at least once"
+    it "should have all manifested files present"
+    it "should have verified checksums of all manifested files"
+  end
+
+  describe "bag-info.txt" do
+    it "should not be empty"
+    it "should contain lines of the format LABEL : VALUE"
+    it "should be case insensitive with respect to LABELs"
+    it "may have folded VALUEs"
+  end
+
+  describe "serialization" do
+    it "should contain only bag"
+    it "should have the same basename as the bag's base dir"
+    it "should produce a single top level bad dir"
+    it "should preserve the validity of the bag"
+  end
+
 end
 
-describe "bagit.txt" do
-  it "should have exaclty two lines"
-  it "should have a bagit version"
-  it "should have a tag file encoding"
-end
 
-describe "manifest-[algorithm].txt" do
-  it "should have valid algorithm in the name (at least md5 and sha1)"
-  it "should only contain lines of the format CHECKSUM FILENAME"
-  it "should only use the slash character as a path separator in FILENAME"
-  it "should only use hex-encoded checksums"
-  it "should contain FILENAMEs that are relative paths from the base directory"
-  it "should have one or more whitespace characters separating fields"
-end
-
-describe "fetch.txt" do
-  it "should only contain lines of the format URL LENGTH FILENAME"
-  it "should only have cannonical URLs"
-  it "should only have a positive integer or - (unspecified) for LENGTH"
-  it "should only use the slash character as a path separator in FILENAME"
-  it "should contain FILENAMEs that are relative paths from the base directory"
-  it "should have one or more whitespace characters separating fields"
-  it "should be renamed when receipt of a bag is complete"
-end
-
-describe "tagmanifest-[algorithm].txt" do
-  it "should work just like a nomral manifest file, but only contain tag files"
-end
-
-describe "a valid bag" do
-  it "should have every present payload file manifested at least once"
-  it "should have all manifested files present"
-  it "should have verified checksums of all manifested files"
-end
-
-describe "bag-info.txt" do
-  it "should not be empty"
-  it "should contain lines of the format LABEL : VALUE"
-  it "should be case insensitive with respect to LABELs"
-  it "may have folded VALUEs"
-end
-
-describe "serialization" do
-  it "should contain only bag"
-  it "should have the same basename as the bag's base dir"
-  it "should produce a single top level bad dir"
-  it "should preserve the validity of the bag"
-end
