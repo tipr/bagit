d7210e0795304583666f9b0d1eafa9caef1ea3ff
Marly <skifly01@mjw2.local>
2009-01-27 09:57:52 -0500

    Updating SHA1 creation to support Ruby 1.8.6

--- tools/lib/tipr.rb
+++ tools/lib/tipr.rb
@@ -1,16 +1,17 @@
 # This module holds a set of helper functions used by scripts and specs
 # to manage various parts of constructing the TIPR package
 
-require 'erb'		# ERB for generating our xml files
-require 'digest/sha1'	# SHA1 library for calculating checksums
+require 'erb'          # ERB for generating our xml files
+require 'digest/sha1'  # SHA1 library for calculating checksums
 
 module TIPR
 
   # Returns xml parsed using a dip object, ERB, and the specified template
   # Template argument should be relative to our template directory
   # FIXME: This is a kludge
+
   def self.generate_xml(template, dip, rep=nil, orig=nil, active=nil)
-    @type = rep if rep	       # Used for typing our template
+    @type = rep if rep         # Used for typing our template
     @orig = orig if orig       # Used for the tipr envelope
     @active = active if active #  ""
     @dip = dip
@@ -23,9 +24,10 @@ module TIPR
   
   # Creates a simple hash of the input file and sha-1 sum.
   # Intended to facilitate generating and looping through representations.
+
   def self.sha1_pair(xml)
     {
-      :sha_1 => Digest::SHA1.new(xml),
+      :sha_1 => Digest::SHA1.hexdigest(xml),
       :xml => xml
     }
   end
