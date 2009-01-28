4f3c1e9a63c1f87fb212e8d3337a436c799f6b1b
marly <marlified@gmail.com>
2009-01-28 16:44:33 -0500

    changing original and current representation determination to meet M2

--- tools/lib/dip.rb
+++ tools/lib/dip.rb
@@ -6,7 +6,7 @@ require 'time'
 
 class DIP
   
-  attr_reader :rel_path, :ieid, :package_id, :create_date, :original_representation, :current_representation
+  attr_reader :rel_path, :ieid, :package_id, :create_date, :original_representation, :current_representation, :migration_map
   
   NS = {
     'mets' => 'http://www.loc.gov/METS/',
@@ -15,12 +15,14 @@ class DIP
   }
 
   def initialize(path)
-    @path = path.chomp('/')  # Strip any trailing /es
+    @path = path.chomp('/')     # Strip any trailing /es
     @doc = load_descriptor
     @rel_path = load_rel_path
     @ieid = load_ieid
     @package_id = load_package_id
     @create_date = load_create_date
+    @dfid_map = load_dfid_map
+    @migration_map = load_migration_map
     @original_representation = load_original_representation
     @current_representation = load_current_representation
   end
@@ -35,7 +37,7 @@ class DIP
     open(descriptor) { |io| Nokogiri::XML io }
   end
   
-  def load_rel_path # includes package_id
+  def load_rel_path             # includes package_id
     matches = Dir.glob "#{@path}/*/AIP_*_LOC.xml"
     dir = File.dirname(matches.first)
     dir.split("DIPs/").last
@@ -59,24 +61,75 @@ class DIP
     Time.parse create_date_node.content
   end
   
-  def load_original_representation
+  def load_dfid_map             # Create a map between non-global DFIDs and FILEIDs
+    file_ids = @doc.xpath('//mets:structMap//mets:fptr/@FILEID', NS).map do |file_id_node| 
+                 file_id_node.content
+               end
+    dfid_map = Hash.new
+    file_ids.uniq.each do |fid|
+      tmd = @doc.xpath("//mets:file[@ID = '#{fid}']", NS).first['ADMID']
+      dfid = @doc.xpath("//mets:techMD[@ID = '#{tmd}']//daitss:DFID", NS).first.content
+      dfid_map[:"#{dfid}"] = fid 
+    end
+    dfid_map
+  end
+  
+  def load_migration_map        # Create a map between migrated FILEIDs
+    mnodes = @doc.xpath('//daitss:REL_TYPE', NS).select { |n| n.content == 'MIGRATED_TO' }
+    return nil if mnodes.empty?	# Don't bother going further  
+    migration_map = Hash.new    # Will hold FILEID migration relationships
     
-    @doc.xpath('//mets:structMap[@ID="SMR0"]//mets:fptr/@FILEID', NS).map do |file_id_node|
-      file_id = file_id_node.content
-      file_node = @doc.xpath("//mets:file[@ID = '#{file_id}']", NS).first
-      {
-        :sha_1 => file_node.attributes['CHECKSUM'],
-        :path => file_node.xpath('mets:FLocat/@xlink:href', NS).first.content
-      }
+    # Look up our DFIDs in our migration relationships, and add their  
+    # corresponding FILEIDs to our migration_map hash.
+
+    mnodes.each do |n|
+
+      # DFIDs
+      old_dfid = n.parent.xpath('./daitss:DFID_1', NS).first.content 
+      new_dfid = n.parent.xpath('./daitss:DFID_2', NS).first.content 
+      
+      # FILEIDs
+      old_file_id = @dfid_map[:"#{old_dfid}"]
+      new_file_id = @dfid_map[:"#{new_dfid}"]
+
+      migration_map[:"#{old_file_id}"] = new_file_id
+
     end
     
+    migration_map
+
+  end
+  
+  def load_original_representation
+    
+    id_list = @doc.xpath('//mets:file', NS)
+    
+    # If we've had a migration, exclude these new files from this representation
+    if not @migration_map.nil? 
+      id_list = id_list.select { |node| not @migration_map.has_value?(node['ID']) }
+    end
+    
+    id_list.map do |file_node| 
+      {
+      	:sha_1 => file_node['CHECKSUM'],
+      	:path => file_node.xpath('mets:FLocat/@xlink:href', NS).first.content
+      }
+    end
+
   end
 
   def load_current_representation
+  
+    return @original_representation if @migration_map.nil? # Original rep is current
     
-    @doc.xpath('//mets:file', NS).map do |file_node|
+    # Exclude older files that we've migrated from this representation
+    id_list = @doc.xpath('//mets:file', NS).select do |file_node| 
+      not @migration_map.member?(:"#{file_node['ID']}")
+    end
+    
+    id_list.map do |file_node| 
       {
-        :sha_1 => file_node.attributes['CHECKSUM'],
+        :sha_1 => file_node['CHECKSUM'],
         :path => file_node.xpath('mets:FLocat/@xlink:href', NS).first.content
       }
     end
