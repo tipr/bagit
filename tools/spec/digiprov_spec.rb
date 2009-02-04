c7f47b0ebd8338c11c2e8686d74c8588485226af
Francesco Lazzarino <flazzarino@gmail.com>
2009-02-04 12:09:09 -0500

    retooled the xml matchers & cleaned up the digiprov spec

--- tools/spec/digiprov_spec.rb
+++ tools/spec/digiprov_spec.rb
@@ -7,105 +7,100 @@ require 'spec_helper'
 describe "the file digiprov descriptor" do
 
   before(:each) do
+        
+    event_doc = Nokogiri::XML <<XML
+<daitss xmlns="http://www.fcla.edu/dls/md/daitss/">
+  <EVENT>
+    <ID>488374</ID>
+    <OID>F20090127_AAAAAA</OID>
+    <EVENT_TYPE>CV</EVENT_TYPE>
+    <DATE_TIME>2009-01-27 14:32:12</DATE_TIME>
+    <EVENT_PROCEDURE>Checked for virus during DataFile creation</EVENT_PROCEDURE>
+    <OUTCOME>SUCCESS</OUTCOME>
+    <NOTE></NOTE>
+  </EVENT>
+  <EVENT>
+    <ID>488375</ID>
+    <OID>F20090127_AAAAAA</OID>
+    <EVENT_TYPE>VC</EVENT_TYPE>
+    <DATE_TIME>2009-01-27 14:32:14</DATE_TIME>
+    <EVENT_PROCEDURE>compareMessageDigests</EVENT_PROCEDURE>
+    <OUTCOME>SUCCESS</OUTCOME>
+    <NOTE>Compared archive-calculated checksum to submitted checksum. Type: sha-1</NOTE>
+  </EVENT>
+  <EVENT>
+    <ID>488376</ID>
+    <OID>F20090127_AAAAAB</OID>
+    <EVENT_TYPE>CV</EVENT_TYPE>
+    <DATE_TIME>2009-01-27 14:32:19</DATE_TIME>
+    <EVENT_PROCEDURE>Checked for virus during DataFile creation</EVENT_PROCEDURE>
+    <OUTCOME>SUCCESS</OUTCOME>
+    <NOTE></NOTE>
+  </EVENT>
+</daitss>
+XML
 
-    # define our namespace
-    @xmlns = { 'xmlns' => 'http://www.loc.gov/standards/premis/v1' } 
-
-    # need a daitss DIP
-    path = File.join '..', 'DIPs', 'FDA0666002'
-    @dip = DIP.new path
-    
-    # need our DAITSS events (we choose the ones in our dip related to the jpg)
-    @events = @dip.events('FID1')
+    @events = event_doc.xpath('//daitss:EVENT', 'daitss' => "http://www.fcla.edu/dls/md/daitss/").to_a
 
     # generate our xml
     t = open File.join('templates', 'digiprov.xml.erb') do |io|
       string = io.read
       ERB.new(string, nil, '<>')
     end
-    
+        
     raw_xml = t.result binding
-    @doc = Nokogiri::XML raw_xml
- 
+    @doc = Nokogiri::XML raw_xml, nil, nil, Nokogiri::XML::PARSE_NOBLANKS
   end
 
-  it "should be a premis file" do
-    @doc.root.name.should eql('premis')
-    @doc.root.should be_in_namespace('http://www.loc.gov/standards/premis/v1')    
-  end
-  
-  it "should have an event section" do
-    @doc.root.xpath('./xmlns:event', @xmlns).should_not be_empty
+  it "should be a premis document" do
+    @doc.should have_xpath('premis:premis')
   end
 
-  it "should have three events" do		# In our jpg case
-    @doc.root.xpath('./xmlns:event', @xmlns).length.should == 3
+  it "should have three event" do
+    @doc.xpath('/premis:premis/premis:event', NS_MAP).size.should == 3
   end
-  
-  describe "each event section" do
- 
+
+  describe "the event" do
+
+    before(:each) do
+      @event = @doc.xpath('/premis:premis/premis:event', NS_MAP).first
+    end
+
     it "should have an eventIdentifier" do
-      @doc.root.should have_xpath('./xmlns:event/xmlns:eventIdentifier', @xmlns)
+      @event.should have_xpath('premis:eventIdentifier')
     end
-    
-    describe "the event identifier" do
-    
-      it "should have an eventIdentifierType" do
-        @doc.root.xpath('//xmlns:eventIdentifier/xmlns:eventIdentifierType', 
-                        @xmlns).should have_node_content("URI")
-      end
-      
-      it "should have an eventIdentifierValue which matches the daitss:event id" do
-        @doc.root.xpath('//xmlns:eventIdentifier/xmlns:eventIdentifierValue', 
-                        @xmlns).should have_node_content(['488374', '488375', '488380'])
-      end
+
+
+    it "should have an eventIdentifierType" do
+      @event.should have_xpath('premis:eventIdentifier/premis:eventIdentifierType')
     end
-    
+
+    it "should have an eventIdentifierValue which matches the daitss:event id" do
+      #@event.should have_xpath('premis:eventIdentifier/premis:eventIdentifierValue["488374"]')
+    end
+
     it "should have an eventType" do
-      @doc.root.xpath('./xmlns:event/xmlns:eventType', 
-        @xmlns).should have_node_content(["Virus check", "Verify checksum", "Migrated"])
+      @event.should have_xpath('premis:eventType["Virus check"]')
     end
 
     it "should have an eventDateTime" do
-
-      # convert real times to required xml schema (80611) format
-      rts = ['2009-01-27 14:32:12', '2009-01-27 14:32:14', '2009-01-27 14:32:35'].map do |s|
-        Time.parse(s).xmlschema
-      end
-      
-      @doc.root.xpath('./xmlns:event/xmlns:eventDateTime', 
-                      @xmlns).should have_node_content(rts)
+      expected_time = Time.parse '2009-01-27 14:32:12'
+      actual_time = Time.parse @event.xpath("premis:eventDateTime", NS_MAP).first.content
+      actual_time.should == expected_time
+      # @event.should have_xpath("premis:eventDateTime[text()='#{ expected_time }']")
     end
-    
+
     it "should have an eventDetail which matches the daitss:procedure" do
-      details = [ "Checked for virus during DataFile creation", "compareMessageDigests",
-                  "Create a jp2 file migrated from a jpeg file" ]
-      @doc.root.xpath('./xmlns:event/xmlns:eventDetail', 
-                      @xmlns).should have_node_content(details)
+      @event.should have_xpath("premis:eventDetail['Checked for virus during DataFile creation']")
     end
-    
+
     it "should have eventOutcomeInformation" do
-      @doc.root.should have_xpath('./xmlns:event/xmlns:eventOutcomeInformation', @xmlns)
+      outcome = @event.xpath('premis:eventOutcomeInformation', NS_MAP).first
+      outcome.should have_xpath('premis:eventOutcome["SUCCESS"]')
+      detail = outcome.xpath('premis:eventOutcomeDetail', NS_MAP).first
+      detail.content.should be_empty
     end
 
-    describe "the eventOutcomeInformation" do
-
-      it "should have eventOutcome which matches the daitss:outcome for the event" do
-        @doc.root.xpath('//xmlns:eventOutcomeInformation/xmlns:eventOutcome', 
-                        @xmlns).should have_node_content("SUCCESS")
-      end
-      
-      it "should have eventOutcomeDetail which matches the daitss:note for the event" do
-        details=[ "", 
-                  "Compared archive-calculated checksum to submitted checksum. Type: sha-1",
-                  "File derived from: /stage/var/daitss/ingest/work/FDA0666002/E20090127_AAAAAA/ateam.jpg" ]
-
-        @doc.root.xpath('//xmlns:eventOutcomeInformation/xmlns:eventOutcomeDetail',
-                        @xmlns).should have_node_content(details)
-      end
-            
-    end
-    
   end
- 
+
 end
--- tools/spec/spec_helper.rb
+++ tools/spec/spec_helper.rb
@@ -1,162 +1,138 @@
-module TIPRMatchers
+ NS_MAP = {
+   'daitss' => "http://www.fcla.edu/dls/md/daitss/",
+   'premis' => 'http://www.loc.gov/standards/premis/v1',
+   'mets' => 'http://www.loc.gov/METS/'
+ }
 
-  class BeInNamespace
+ module TIPRMatchers
+   
+   class BeInNamespace
+     
+     def initialize(expected)
+       @expected = expected        # a namespace uri
+     end
 
-    def initialize(expected)
-      @expected = expected        # a namespace uri
-    end
+     def matches?(target)
+       @target = target            # a node
+       prefix = @target.namespace
+       @target_namespace = @target.namespaces[ prefix ? "xmlns:#{prefix}" : 'xmlns' ]
+       @target_namespace == @expected
+     end
 
-    def matches?(target)
-      @target = target            # a node
-      prefix = @target.namespace
-      @target_namespace = @target.namespaces[ prefix ? "xmlns:#{prefix}" : 'xmlns' ]
-      @target_namespace == @expected
-    end
+     def failure_message
+       "expected <#{@target_namespace}> to be " +
+         "the same as <#{@expected}>"
+     end
 
-    def failure_message
-      "expected <#{@target_namespace}> to be " +
-        "the same as <#{@expected}>"
-    end
+     def negative_failure_message
+       "expected <#{@target_namespace}> not to be " +
+         "the same as <#{@expected}>"
+     end
 
-    def negative_failure_message
-      "expected <#{@target_namespace}> not to be " +
-        "the same as <#{@expected}>"
-    end
+   end
 
-  end
+   class ReferenceAFile
 
-  class ReferenceAFile
+     # Assume an XML file, unless otherwise specified.
+     def initialize(pattern=/\.xml\Z/i, type='XML')
+       @pattern = pattern        # The REGEX we're searching for
+       @type = type              # A pretty name to describe the file type
+     end
 
-    # Assume an XML file, unless otherwise specified.
-    def initialize(pattern=/\.xml\Z/i, type='XML')
-      @pattern = pattern        # The REGEX we're searching for
-      @type = type              # A pretty name to describe the file type
-    end
+     def matches?(target)
+       @target = target          # a node with an href
+       return false if not @target
 
-    def matches?(target)
-      @target = target          # a node with an href
-      return false if not @target
-      
-      @ref = @target['href']
-      return false if not @ref
+       @ref = @target['href']
+       return false if not @ref
 
-      @ref.match(@pattern) != nil
-    end
+       @ref.match(@pattern) != nil
+     end
 
-    def failure_message
-      "expected <#{@ref}> to be " +
-        "a(n) <#{@type}> file"
-    end
+     def failure_message
+       "expected <#{@ref}> to be " +
+         "a(n) <#{@type}> file"
+     end
 
-    def negative_failure_message
-      "expected <#{@ref}> not to be " +
-        "a(n) <#{@type}> file"
-    end
+     def negative_failure_message
+       "expected <#{@ref}> not to be " +
+         "a(n) <#{@type}> file"
+     end
 
-  end
+   end
 
-  class HaveXPath
+   class HaveXPath
 
-    def initialize(xpath, ns)  
-      @xpath = xpath            # The XPath we would like to match.
-      @ns = ns                  # The namespace to use
-    end
+     def initialize(xpath, ns)
+       @xpath = xpath            # The XPath we would like to match.
+       @ns = ns                  # The namespace to use
+     end
 
-    def matches?(target)
-      @target = target          # a node that should contain the xpath
-      not @target.xpath(@xpath, @ns).empty?
-    end
+     def matches?(target)
+       @target = target          # a node that should contain the xpath
+       not @target.xpath(@xpath, @ns).empty?
+     end
 
-    def failure_message
-      "expected xpath <#{@xpath}> to be in " +
-        "node <#{@target}>"
-    end
+     def failure_message
+       "expected xpath <#{@xpath}> to be in " +
+         "node <#{@target}>"
+     end
 
-    def negative_failure_message
-      "expected xpath <#{@xpath}> not to be " +
-        "in node <#{@target}>"
-    end
+     def negative_failure_message
+       "expected xpath <#{@xpath}> not to be " +
+         "in node <#{@target}>"
+     end
 
-  end
-  
-  class HaveNodeContent
+   end
 
-    def initialize(content)  
-      @content = content        # Content: either an array or a single value
-    end
+   def have_xpath(xpath, ns=NS_MAP)
+     HaveXPath.new(xpath, ns)
+   end
 
-    def matches?(target)
-      @target = target          # a NodeSet that should have matching content
-      node_content = @target.map { |n| n.content.strip }
-      return node_content == @content if @content.is_a?(Array)
-      node_content.uniq == [@content]
-    end
+   def reference_an_xml_file()
+     ReferenceAFile.new()
+   end
 
-    def failure_message
-      "expected NodeSet <#{@target}> to have content " +
-        "<#{@values}>"
-    end
+   def reference_a_file()
+     ReferenceAFile.new(/.+/, 'UNRESTRICTED TYPE OF')
+   end
 
-    def negative_failure_message
-      "expected NodeSet <#{@target}> not to have " +
-        "content <#{@values}>"
-    end
+   def be_in_namespace(expected)
+     BeInNamespace.new(expected)
+   end
 
-  end
-  
-  def have_node_content(values)
-    HaveNodeContent.new(values)
-  end
-  
-  def have_xpath(xpath, ns)
-    HaveXPath.new(xpath, ns)
-  end
+ end
 
-  def reference_an_xml_file()
-    ReferenceAFile.new()
-  end
+ module BagitMatchers
 
-  def reference_a_file()
-    ReferenceAFile.new(/.+/, 'UNRESTRICTED TYPE OF')
-  end
+   class BeIn
 
-  def be_in_namespace(expected)
-    BeInNamespace.new(expected)
-  end
+     def initialize(*expected_collection)
+       @expected = expected_collection
+     end
 
-end
+     def matches?(target)
+       @target = target
+       @expected.include? @target
+     end
 
+     def failure_message
+       "expected <#{@target}> to be in collection <#{@expected}>"
+     end
 
-module BagitMatchers
+     def negative_failure_message
+       "expected <#{@target}> to not be in collection <#{@expected}>"
+     end
 
-  class BeIn
+   end
 
-    def initialize(*expected_collection)
-      @expected = expected_collection
-    end
-    
-    def matches?(target)
-      @target = target
-      @expected.include? @target
-    end
+   def be_in(*expected_collection)
+     BeIn.new(*expected_collection)
+   end
 
-    def failure_message
-      "expected <#{@target}> to be in collection <#{@expected}>"
-    end
+ end
 
-    def negative_failure_message
-      "expected <#{@target}> to not be in collection <#{@expected}>"
-    end
-
-  end
-
-  def be_in(*expected_collection)
-    BeIn.new(*expected_collection)
-  end
-
-end
-
-Spec::Runner.configure do |config|
-  config.include(TIPRMatchers)
-  config.include(BagitMatchers)
-end
+ Spec::Runner.configure do |config|
+   config.include(TIPRMatchers)
+   config.include(BagitMatchers)
+ end
