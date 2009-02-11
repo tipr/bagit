feb539a3f515d5376609ceb71b1c2a18a1adb959
Marly Wilson <marlified@gmail.com>
2009-02-11 11:33:39 -0500

    speccing for required object element

--- tools/spec/digiprov_spec.rb
+++ tools/spec/digiprov_spec.rb
@@ -7,7 +7,7 @@ require 'spec_helper'
 describe "the file digiprov descriptor" do
 
   before(:each) do
-        
+
     event_doc = Nokogiri::XML <<XML
 <daitss xmlns="http://www.fcla.edu/dls/md/daitss/">
   <EVENT>
@@ -43,22 +43,45 @@ XML
     @events = event_doc.xpath('//daitss:EVENT', 'daitss' => "http://www.fcla.edu/dls/md/daitss/").to_a
     @premis_schema = "http://www.loc.gov/standards/premis/v1/PREMIS-v1-1.xsd"
     
-    # generate our xml
-    t = open File.join('templates', 'digiprov.xml.erb') do |io|
-      string = io.read
-      ERB.new(string, nil, '<>')
-    end
-        
-    raw_xml = t.result binding
+    raw_xml = TIPR.generate_digiprov("digiprov.xml.erb", @events)
     @doc = Nokogiri::XML raw_xml, nil, nil, Nokogiri::XML::PARSE_NOBLANKS
   end
 
   it "should be a valid premis document" do
     @doc.should have_xpath('premis:premis')
-    TIPR.validates?(@doc.to_xml, LibXML::XML::Schema.new(@premis_schema)).should be_true
+    TIPR.validate(@doc.to_xml, LibXML::XML::Schema.new(@premis_schema)).should be_true
   end
 
-  it "should have three event" do
+  it "should have one object" do
+    @doc.xpath('premis:premis/premis:object', NS_MAP).size.should == 1
+  end
+  
+  describe "the object" do
+
+    before(:each) do
+      @object = @doc.xpath('/premis:premis/premis:object', NS_MAP).first
+    end
+  
+    it "should have an objectIdentifier" do
+      @object.should have_xpath('premis:objectIdentifier')
+    end
+  
+    it "should have an objectIdentifierType" do
+      @object.should have_xpath_with_content('premis:objectIdentifier/premis:objectIdentifierType', "DAITSS")
+    end
+    
+    it "should have an objectIdentifierValue" do
+      @object.should have_xpath_with_content('premis:objectIdentifier/premis:objectIdentifierValue', "F20090127_AAAAAA")
+    end
+
+    it "should have an objectCategory" do
+      @object.should have_xpath_with_content('premis:objectCategory', "file")
+    end
+  
+  end
+  
+  it "should have three events" do
+    puts @doc
     @doc.xpath('/premis:premis/premis:event', NS_MAP).size.should == 3
   end
 
