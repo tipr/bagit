9b34010fdc155be0221323c78782ddfe48c99d1b
roryegerton <roryegerton@gmail.com>
2014-10-01 11:16:29 +0100

    revet master to its original form when first forked

--- lib/bagit/valid.rb
+++ lib/bagit/valid.rb
@@ -14,10 +14,6 @@ module BagIt
     # covered.
     def complete?
 
-      if manifest_files == []
-        errors.add  :completeness, "there are no manifest files"
-      end
-
       unmanifested_files.each do |file|
         errors.add :completeness, "#{file} is present but not manifested"
       end
